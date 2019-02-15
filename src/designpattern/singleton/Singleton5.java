c5792c8766aa63cffb38c1be5dd0c1d89c8e344d
chenglvpeng <Clp123456>
2019-02-15 17:47:52 +0800

    提交

--- src/designpattern/singleton/Singleton5.java
+++ src/designpattern/singleton/Singleton5.java
@@ -6,5 +6,9 @@ package designpattern.singleton;
  * @Description:
  * @注意：本内容仅限于杭州阿拉丁信息科技股份有限公司内部传阅，禁止外泄以及用于其他的商业目的
  */
-public class Singleton5 {
+public enum  Singleton5 {
+    INSTANCE;
+    public void method() {
+
+    }
 }
